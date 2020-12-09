d954acd60157e64aadb42ec3ce02558a5d988267
peterennis <peterennis@yahoo.com>
2020-12-08 21:35:45 -0800

    feat(typescript) upgrade to adaept project using typescript

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        an adaept <img src="/favicon.png" alt="adaept logo" className={styles.logo} /> project
       </footer>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Moonbeam Development!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
         <Header title="Welcome to moonbeam!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>pages/index.tsx</code>
         </p>
       </main>
 
