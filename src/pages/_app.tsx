a2f6fbeabd12274d958c29f50e35507ec35aeb90
peterennis <peterennis@yahoo.com>
2020-12-19 20:24:57 -0800

    revert(): back to v0.4.1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,49 +1,3 @@
-import React, { useEffect } from "react";
-import { defineCustomElements as ionDefineCustomElements } from "@ionic/core/loader";
-
-/* Core CSS required for Ionic components to work properly */
-import "@ionic/core/css/core.css";
-
-/* Basic CSS for apps built with Ionic */
-import "@ionic/core/css/normalize.css";
-import "@ionic/core/css/structure.css";
-import "@ionic/core/css/typography.css";
-
-/* Optional CSS utils that can be commented out */
-import "@ionic/core/css/padding.css";
-import "@ionic/core/css/float-elements.css";
-import "@ionic/core/css/text-alignment.css";
-import "@ionic/core/css/text-transformation.css";
-import "@ionic/core/css/flex-utils.css";
-import "@ionic/core/css/display.css";
-
-function App({ Component, pageProps }) {
-  useEffect(() => {
-    ionDefineCustomElements(window);
-  });
-  return (
-    <ion-app>
-      <ion-header translucent>
-        <ion-toolbar>
-          <ion-title>Next.js with Ionic</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ion-content fullscreen>
-        <Component {...pageProps} />
-      </ion-content>
-      <ion-footer>
-        <ion-toolbar>
-          <ion-title>Footer</ion-title>
-        </ion-toolbar>
-      </ion-footer>
-    </ion-app>
-  );
-}
-
-export default App;
-
-/*
 import { AppProps } from 'next/app'
 
 import '../styles/globals.css'
@@ -53,4 +7,3 @@ function App({ Component, pageProps }: AppProps) {
 }
 
 export default App
-*/
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,26 @@
-import Head from "next/head";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-
-import Image from "next/image";
+import Head from 'next/head'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <Head>
         <title>Moonbeam Development!</title>
         <link rel="icon" href="/assets/icon/favicon.png" />
       </Head>
 
       <main>
+      <div>
+        <img src="/assets/ae-octocat-red.png" alt="cool katze" />
+      </div>
         <Header title="Welcome to moonbeam!" />
         <p className="description">
           Get started by editing <code>pages/index.tsx</code>
         </p>
-
-        <ion-grid>
-          <ion-row>
-            {new Array(8).fill("").map((k, i) => (
-              <ion-col key={i} size="3">
-                <ion-card>
-                  <Image
-                    src="/ae-octocat-red.png"
-                    alt="cool katze"
-                    width={500}
-                    height={500}
-                  />
-                  <ion-card-header>
-                    <ion-card-subtitle>Location</ion-card-subtitle>
-                    <ion-card-title>Kool Katze, CA</ion-card-title>
-                  </ion-card-header>
-                  <ion-card-content>
-                    <ion-icon name="glasses" slot="start"></ion-icon>
-                    No matter what you say, we'll take that challenge any day.
-                    We're the cool, cool, coolest cats in town.
-                  </ion-card-content>
-                </ion-card>
-              </ion-col>
-            ))}
-          </ion-row>
-        </ion-grid>
       </main>
 
       <Footer />
     </div>
-  );
+  )
 }
